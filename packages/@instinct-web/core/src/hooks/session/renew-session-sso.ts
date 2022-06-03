b129a107d344b80814157ee8d76ba8ac7742da17
Chris Pettyjohn <chrismmpettyjohn>
2022-06-03 02:48:34 -0500

    Fix bug causing SSO to double generate
    Former-commit-id: 88a92aa964c44707659e7cf3a202f830b8c0b8b0

--- packages/@instinct-web/core/src/hooks/session/renew-session-sso.ts
+++ packages/@instinct-web/core/src/hooks/session/renew-session-sso.ts
@@ -4,21 +4,21 @@ import {sessionContext} from '../../context/session';
 import {sessionService} from '../../services/session';
 
 export function useRenewSessionSSO() {
-  const {user, sso, setSSO} = useContext(sessionContext);
+  const {user, setUser, sso, setSSO} = useContext(sessionContext);
   useEffect(() => {
     if (sso) {
-      console.log('SSO already generated.  Skipping creation.');
       return;
     }
 
     async function fetchSSO() {
-      console.log('Fetching new SSO');
       const currentUserStatus = await sessionService.getCurrentUser();
 
       if (currentUserStatus.online) {
         toast.error("You're already online!");
         return;
       }
+
+      setUser({online: true});
 
       const sso = await sessionService.createSSO();
       setSSO(sso);
